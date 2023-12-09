3b373f3bd8059b469ace189b8d5cd33abd62967c
OQueLucas <lucasestevam365@gmail.com>
2023-12-09 20:36:40 -0300

    Create son(dumb) component and set input from parent

---
+++ src/app/modules/components/weather-card/weather-card.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { WeatherDatas } from 'src/app/models/interfaces/WeatherDatas.interface';
+
+@Component({
+  selector: 'app-weather-card',
+  templateUrl: './weather-card.component.html',
+  styleUrls: [],
+})
+export class WeatherCardComponent implements OnInit {
+  @Input() weatherDatasInput!: WeatherDatas;
+
+  ngOnInit(): void {
+    console.log(
+      'Dados recebidos do componente pai - weather-card: ',
+      this.weatherDatasInput
+    );
+  }
+}
